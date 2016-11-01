462f62b03d206a84301075c3458c001baa82f00e
cwm262 <azathoth.hp@gmail.com>
2016-11-01 15:56:06 -0500

    Pushing up remaining changes from nov 1 meeting for storyboard

--- Meeting Notes/NoteViewController.swift
+++ Meeting Notes/NoteViewController.swift
@@ -11,9 +11,22 @@ import UIKit
 class NoteViewController: UIViewController {
 
     @IBOutlet weak var startTimeBtn: UIButton!
+    @IBOutlet weak var endTimeBtn: UIButton!
+    @IBOutlet weak var startDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //Setting cell detail with current date as string
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM d, yyyy    h:mm a"
+        let date = Date(timeIntervalSinceNow: 0)
+        let dateString = dateFormatter.string(from: date)
+        startTimeBtn.setTitle(dateString, for: .normal)
+        
+        let endDate = Date(timeIntervalSinceNow: 3600)
+        let endDateString = dateFormatter.string(from: endDate)
+        endTimeBtn.setTitle(endDateString, for: .normal)
         
     }
 
@@ -22,6 +35,9 @@ class NoteViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func startTimeClicked(_ sender: Any) {
+        
+    }
 
     /*
     // MARK: - Navigation
